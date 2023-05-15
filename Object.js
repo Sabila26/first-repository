3c34cc611480f718b98b09859854785d5b712825
Sabila26 <127819818+Sabila26@users.noreply.github.com>
2023-05-15 20:44:05 +0700

    latihan modul 4

---
+++ Object.js
@@ -0,0 +1,17 @@
+ const user1 = {
+               firstname: "Sabila",
+                lastname: "Mawarda",
+                age: "20",
+            };
+            console.log('Hallo..., Nama Saya  $(user1.firstname) $(user1.lastname)')
+            console.log('Umur Saya $(user1.age) tahun')
+
+            const user2 = {
+                kuliah: "Polibang",
+                jurusan: "Rekayasa Perangkat Lunak",
+                nim: "2022010031",
+            };
+            console.log('Kuliah di $(user2.kuliah)')
+            console.log('Prodi $(user2.jurusan) (RPL)')
+            console.log('Nomor NIM $(user2.nim)')
+       
